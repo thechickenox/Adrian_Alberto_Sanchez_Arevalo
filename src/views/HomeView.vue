edc4dd5243fefff6f093b36ffeb6d5415a6dd0ac
thechickenox <thechickenox117@gmail.com>
2024-01-31 20:35:06 -0500

    Correcion

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -44,46 +44,79 @@ const submitForm = () => {
 </script>
 <template>
   <main>
-    <div>
-    <form  @submit.prevent="submitForm">
-      <label class="color" for="nombre">Nombre:</label>
-      <input type="text" id="nombre" v-model="nombre" @input="validateNombre">
-      <span v-if="!nombreValido">El nombre debe tener 5 caracteres.</span>
+    <div class="margen">
+      <form @submit.prevent="submitForm">
+        <label class="color" for="nombre">Nombre:</label>
+        <input type="text" id="nombre" v-model="nombre" @input="validateNombre">
+        <span class="color" v-if="!nombreValido">El nombre debe tener 5 caracteres.</span>
 
-      <br>
+        <br>
 
-      <label for="apellido">Apellido:</label>
-      <input type="text" id="apellido" v-model="apellido" @input="validateApellido">
-      <span v-if="!apellidoValido">El apellido no debe ser igual al nombre.</span>
+        <label  class="color" for="apellido">Apellido:</label>
+        <input type="text" id="apellido" v-model="apellido" @input="validateApellido">
+        <span class="color" v-if="!apellidoValido">El apellido no debe ser igual al nombre.</span>
 
-      <br>
+        <br>
 
-      <label for="edad">Edad:</label>
-      <input type="number" id="edad" v-model="edad" @input="validateEdad">
-      <span v-if="!edadValida">La edad debe ser mayor a 0 y menor a 60.</span>
+        <label  class="color" for="edad">Edad:</label>
+        <input type="number" id="edad" v-model="edad" @input="validateEdad">
+        <span class="color" v-if="!edadValida">La edad debe ser mayor a 0 y menor a 60.</span>
 
-      <br>
+        <br>
 
-      <label for="genero">Género:</label>
-      <select id="genero" v-model="genero" @change="validateGenero">
-        <option value="">Selecciona una opción</option>
-        <option value="masculino">Masculino</option>
-        <option value="femenino">Femenino</option>
-        <option value="otro">Otro</option>
-      </select>
-      <input type="text" v-if="genero === 'otro'" v-model="otroGenero" placeholder="Ingrese otro género">
-      <span v-if="!generoValido">Por favor, selecciona un género válido.</span>
+        <label  class="color" for="genero">Género:</label>
+        <select id="genero" v-model="genero" @change="validateGenero">
+          <option value="">Selecciona una opción</option>
+          <option value="masculino">Masculino</option>
+          <option value="femenino">Femenino</option>
+          <option value="otro">Otro</option>
+        </select>
+        <input type="text" v-if="genero === 'otro'" v-model="otroGenero" placeholder="Ingrese otro género">
 
-      <br>
+        <br>
 
-      <button type="submit">Enviar</button>
-      
-    </form>
-  </div>
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
   </main>
 </template>
 
 <style scoped>
 
+  .margen {
+    text-align: center;
+    margin-left: 50px;
+    margin-right: auto;
+    max-width: 800px; /* Ajusta el ancho máximo según tu preferencia */
+    padding: 100px;
+    background-color: #f0f0f0;
+    border-radius: 8px;
 
+  }
+
+  .color {
+    color: #333;
+    font-weight: bold;
+  }
+
+  input,
+  select {
+    margin-bottom: 10px;
+  }
+
+  button {
+    background-color: #007bff;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    
+  }
+
+  button:disabled {
+    background-color: #d3d3d3;
+    cursor: not-allowed;
+  }
 </style>
+
