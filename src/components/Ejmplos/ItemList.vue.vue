48882bb510c52daa04999459d9eb54e28142b9cf
thechickenox <thechickenox117@gmail.com>
2024-02-10 11:56:34 -0500

    Modificacion al codigo

--- src/components/Ejmplos/ItemList.vue.vue
+++ src/components/Ejmplos/ItemList.vue.vue
@@ -1,6 +1,7 @@
 <template>
   <ul>
-    <li v-for="item in data" :key="item.id" @click="handleItemClick(item.id)">
+    <li v-for="item in data" :key="item.id" @click="handleItemClick(item.id)"
+      :style="{ color: item.textColor, backgroundColor: item.bgColor }">
       {{ item.value }}
     </li>
   </ul>
@@ -12,13 +13,27 @@ export default defineComponent({
   name: 'ItemList',
   props: {
     data: {
-      type: Array as () => Array<{ id: number; value: string }>,  // Importación de tipo única
+      type: Array as () => Array<{ id: number; value: string; textColor?: string; bgColor?: string }>,
       required: true,
     },
   },
   methods: {
     handleItemClick(id: number) {
+      this.data.forEach((item) => {
+        if (item.id === id) {
+          item.textColor = this.getRandomColor();
+          item.bgColor = this.getRandomColor();
+        }
+      });
       this.$emit('item-clicked', id);
+    },
+    getRandomColor() {
+      const letters = '0123456789ABCDEF';
+      let color = '#';
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
     },
   },
 });
