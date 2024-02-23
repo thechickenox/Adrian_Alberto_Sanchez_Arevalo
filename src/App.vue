f8e5215cc600ec751f1497b59022c9fd7341908a
thechickenox <thechickenox117@gmail.com>
2024-02-22 19:37:01 -0500

    Ultimos_cambios

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
-<<<<<<< HEAD
 import ProjectList from './components/ProyectDT/ProjectList.vue';
-=======
-import {  RouterView } from 'vue-router'
->>>>>>> origin/ExamenU1
 
 </script>
 
 <template>
   <header>
-<<<<<<< HEAD
     
   </header>
   <ProjectList>
@@ -17,19 +12,10 @@ import {  RouterView } from 'vue-router'
         Lista de tareas
       </template>
   </ProjectList>
-=======
-
-  </header>
-  <div>
-    <router-view />
-
-  </div>
->>>>>>> origin/ExamenU1
 
 </template>
 
 <style scoped>
-<<<<<<< HEAD
 
 
 </style>
@@ -38,67 +24,3 @@ import {  RouterView } from 'vue-router'
 
 
 
-=======
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
->>>>>>> origin/ExamenU1
